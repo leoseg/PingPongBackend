1a0b5930a32c0480a77df29acfdecaf6d380d5d1
leoseg <leoseeger16@googlemail.com>
2023-01-21 18:09:28 +0100

    Initial commit

---
+++ src/main/java/com/pingpong/pingpongbackend/PingPongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pingpong.pingpongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingPongBackendApplication.class, args);
+    }
+
+}
